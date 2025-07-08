48ec5111b160fa6c85e19f73ce021df8621cb7b9
MrFord87 <a.ford@fsgsolutions.net>
2025-07-08 00:40:46 -0500

    Create homepage MVP

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1>Welcome to Roza FSG MVP 🚀</h1>
+    </div>
+  );
+}
