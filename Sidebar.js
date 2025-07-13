213ae696c58e1bace9d84a06191f644e06ccaa9f
MrFord87 <a.ford@fsgsolutions.net>
2025-07-12 20:01:18 -0500

    Update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -1,13 +1,1 @@
-export default function Sidebar() {
-  return (
-    <div style={{ width: '250px', backgroundColor: '#222', color: 'white', padding: '1rem' }}>
-      <h2>Roza</h2>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li>Dashboard</li>
-        <li>Opportunities</li>
-        <li>Saved</li>
-        <li>Team</li>
-      </ul>
-    </div>
-  );
-}
+
