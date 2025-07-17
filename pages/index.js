40c2173f03c1a2004cd5213cb177d6f2fcdbc253
MrFord87 <a.ford@fsgsolutions.net>
2025-07-16 20:16:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      fontFamily: 'Arial' 
-    }}>
-      <h1>🚀 Welcome to Roza FSG — It’s ALIVE!</h1>
+    <div style={{ display: 'flex', height: '100vh', backgroundColor: '#111' }}>
+      <Sidebar />
+      <div style={{ flex: 1, color: 'white' }}>
+        <Header />
+        <div style={{ padding: '2rem' }}>
+          <h1>Welcome to Roza, your Contract Intelligence Dashboard</h1>
+        </div>
+      </div>
     </div>
   );
 }
