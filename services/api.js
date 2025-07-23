72fd8841e66b1d3770f69616463103bc81c5f100
MrFord87 <a.ford@fsgsolutions.net>
2025-07-23 17:23:24 -0500

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,1 +1,15 @@
+// services/api.js
 
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.sam.gov', // Replace this baseURL for each source (we’ll handle per-endpoint if needed)
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  params: {
+    api_key: 'jOZCfNRzt6nwN7jb5oWRSC5qFw9nz67QV0Rclc3x',
+  },
+});
+
+export default api;
