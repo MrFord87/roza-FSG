0c89458eb5a1d5b5ddc42f1e49d43db0ad5be28c
MrFord87 <a.ford@fsgsolutions.net>
2025-07-28 22:12:06 -0500

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = 'jOZCfNRzt6nwN7jb5oWRSC5qFw9nz67QV0Rclc3x';
 
 export async function fetchSAMData(keyword) {
-  const url = `https://api.sam.gov/opportunities/v1/search?api_key=${API_KEY}&keyword=${encodeURIComponent(keyword)}&limit=10&sort=modifiedDate`;
+  const url = `https://api.sam.gov/opportunities/v1/search?api_key=${API_KEY}&q=${encodeURIComponent(keyword)}&limit=10&sort=modifiedDate`;
 
   try {
     const response = await axios.get(url);
