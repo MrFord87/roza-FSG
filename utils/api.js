04b4ca8f9f7e34c7a84c87166ca7f36904f7798c
MrFord87 <a.ford@fsgsolutions.net>
2025-07-31 16:58:58 -0500

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-const API_KEY = 'GAPIibFeKRJPKpjkhxUlCRU1fjkynbAQ2tfyMVEj'; // Your SAM.gov API Key
+const API_KEY = 'GAPIibFeKRJPKpjkhxUlCRU1fjkynbAQ2tfyMVEj'; 
 
 export async function fetchSAMData(keyword, location, naics) {
-  const baseUrl = 'https://api.sam.gov/opportunities/v1/search';
+  const baseUrl = 'https://api.sam.gov/opportunities/v2/search';
   const params = new URLSearchParams();
 
   params.append('api_key', API_KEY);
   if (keyword) params.append('q', keyword);
   if (location) params.append('placeOfPerformance.stateCode', location);
   if (naics) params.append('naics', naics);
+  params.append('postedFrom', '07/01/2025'); 
+  params.append('postedTo', '07/31/2025');   
   params.append('sort', 'modifiedDate');
   params.append('limit', '10');
 
