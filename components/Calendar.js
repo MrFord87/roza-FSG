00e455892060696f6720cf6f5714b9e6ef0adb1c
MrFord87 <a.ford@fsgsolutions.net>
2025-08-07 02:06:15 -0500

    Update Calendar.js

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,23 +1,21 @@
-// /components/Calendar.js
 import React, { useState } from 'react';
-import './calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-// Setup localizer
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
   const [events, setEvents] = useState([
     {
-      title: 'Example Event',
+      title: 'Demo Event',
       start: new Date(),
       end: new Date(),
     },
   ]);
 
   return (
-    <div style={{ height: '80vh', padding: '20px' }}>
+    <div style={{ height: '80vh', padding: '1rem' }}>
       <Calendar
         localizer={localizer}
         events={events}
