baa15b12c2f7f421952ceeeb958ef1db4edd47c3
MrFord87 <a.ford@fsgsolutions.net>
2025-08-07 00:08:21 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import Login from './login';
 
 export default function Home() {
-  return <Login />;
+  return (
+    <div className="dark">
+      <Login />
+    </div>
+  );
 }
