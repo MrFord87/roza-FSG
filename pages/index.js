41175c1395ce6823f26f950559da85584e35d232
MrFord87 <a.ford@fsgsolutions.net>
2025-08-07 00:39:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Login from './login';
 
 export default function Home() {
   return (
-    <div className="dark">
-      <Login />
+    <div className="min-h-screen bg-black text-white">
+      <div className="text-red-500 text-2xl p-8">Tailwind is now working 🎉</div>
     </div>
   );
 }
