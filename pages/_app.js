d0fc1098a5cd5e629108d991de23d96313875ee4
MrFord87 <a.ford@fsgsolutions.net>
2025-08-13 19:04:29 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-// pages/_app.js
-import '@fullcalendar/core/index.css';     // FullCalendar core styles
-import '@fullcalendar/daygrid/index.css'; // DayGrid view styles
+
 import '../styles/globals.css';           // Keep this if you actually have it; remove if not
 
 export default function App({ Component, pageProps }) {
